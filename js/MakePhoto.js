e93ed71bef46c2e93ffed012b2ab4a284a24ed5f
Zeks1990 <zhaoxu942698@163.com>
2017-04-30 13:38:39 +0800

    同步代码

--- js/MakePhoto.js
+++ js/MakePhoto.js
@@ -13,7 +13,7 @@ $(function () {
 	const MAX_HEIGHT = canvas.height;
 	const MAX_WIDTH = canvas.width;
 	uploadPhoto.change(readFile);
-	downLoadImg.click(canvasToImage);
+	downLoadImg.click(getBrowser() == "ie" ? IECanvasToImage : canvasToImage);
 	//获取上传图片
 	function readFile () {
 		var file = this.files[0]; // 获取input输入的图片
@@ -105,12 +105,18 @@ $(function () {
 			});
 		}
 	});
+
 	//下载图片
-	function canvasToImage(){
+
+	function refreshCanvas () {
 		context.clearRect(0, 0, MAX_WIDTH, MAX_HEIGHT);
 		contextUp.clearRect(0, 0, MAX_WIDTH, MAX_HEIGHT);
 		context.drawImage(img, drawX, drawY, img.width, img.height);
 		context.drawImage(mask, 0, 0, MAX_WIDTH, MAX_HEIGHT);
+	}
+
+	function canvasToImage () {
+		refreshCanvas();
 		/*下载方案一*/
 		var dom = document.createElement("a");
 		dom.href = canvas.toDataURL('image/png');
@@ -143,13 +149,10 @@ $(function () {
 
 	/***********IE下载***********/
 
-	document.getElementById('saveButton').addEventListener('click', IECanvasToImage, false);
+	//document.getElementById('saveButton').addEventListener('click', IECanvasToImage, false);
     /*----------------------------------------------------------------------------------------------------------------------------------------------------------*/
-    function IECanvasToImage(evt) {
-		context.clearRect(0, 0, MAX_WIDTH, MAX_HEIGHT);
-		contextUp.clearRect(0, 0, MAX_WIDTH, MAX_HEIGHT);
-		context.drawImage(img, drawX, drawY, img.width, img.height);
-		context.drawImage(mask, 0, 0, MAX_WIDTH, MAX_HEIGHT);
+    function IECanvasToImage (evt) {
+		refreshCanvas();
 	    evt.preventDefault(); // Do not refresh the page when the Submit button is clicked.
 	    window.BlobBuilder = window.BlobBuilder || window.MSBlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
 	    canvas.toBlob = canvas.toBlob || canvas.msToBlob;      
@@ -161,4 +164,41 @@ $(function () {
 	    	window.navigator.saveBlob(blobBuilderObject.getBlob(), filename); // Move the builder object content to a blob and save it to a file.      
 	    }
     }
+
+    /***判断浏览器***/
+	function getBrowser (getVersion) {
+		var ua_str = navigator.userAgent.toLowerCase(), ie_Tridents, trident, match_str, ie_aer_rv, browser_chi_Type;
+		//判断IE 浏览器
+		if("ActiveXObject" in self){
+			// ie_aer_rv:  指示IE 的版本.
+			// It can be affected by the current document mode of IE.
+			ie_aer_rv= (match_str = ua_str.match(/msie ([\d.]+)/)) ?match_str[1] :
+			(match_str = ua_str.match(/rv:([\d.]+)/)) ?match_str[1] : 0;
+
+			// ie: Indicate the really version of current IE browser.
+			ie_Tridents = {"trident/7.0": 11, "trident/6.0": 10, "trident/5.0": 9, "trident/4.0": 8};
+			//匹配 ie8, ie11, edge
+			trident = (match_str = ua_str.match(/(trident\/[\d.]+|edge\/[\d.]+)/)) ?match_str[1] : undefined;
+			browser_chi_Type = (ie_Tridents[trident] || ie_aer_rv) > 0 ? "ie" : undefined;
+		}else{
+			//判断 windows edge 浏览器
+			// match_str[1]: 返回浏览器及版本号,如: "edge/13.10586"
+			// match_str[1]: 返回版本号,如: "edge" 
+			//若要返回 "edge" 请把下行的 "ie" 换成 "edge"。 注意引号及冒号是英文状态下输入的
+			browser_chi_Type = (match_str = ua_str.match(/edge\/([\d.]+)/)) ? "ie" :
+			//判断firefox 浏览器
+			(match_str = ua_str.match(/firefox\/([\d.]+)/)) ? "firefox" : 
+			//判断chrome 浏览器
+			(match_str = ua_str.match(/chrome\/([\d.]+)/)) ? "chrome" : 
+			//判断opera 浏览器
+			(match_str = ua_str.match(/opera.([\d.]+)/)) ? "opera" : 
+			//判断safari 浏览器
+			(match_str = ua_str.match(/version\/([\d.]+).*safari/)) ? "safari" : undefined;
+		}   
+		//返回浏览器类型和版本号
+		var verNum, verStr;
+		verNum = trident && ie_Tridents[trident] ? ie_Tridents[trident] : match_str[1];
+		verStr = (getVersion != undefined) ? browser_chi_Type+"/"+verNum : browser_chi_Type;
+		return verStr;
+	}
 });
